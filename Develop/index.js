cb4d8c6617244f4402409087ad645fb2ecbc897b
shimna <chimmu.p@gmail.com>
2023-07-19 14:25:17 +1000

    created the prompt interface

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,83 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown=require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = ["What is your GitHub username ? ","What is your email address ? ","What is your project's name ?","Please write a short description of your project.","What kind of license should your project have ?","What command should be run to install dependencies ? ","What command should be run to run tests ?","What does the user need to know about using the repo ? ","Contributing to the repo ?"];
-const[username,emailId,title,description,license,dependencies,tests,usage,Contribution]=questions;
+const questions = ["What is your GitHub username ? ", "What is your email address ? ", "What is your project's name ?", "Please write a short description of your project.", "What kind of license should your project have ?", "What command should be run to install dependencies ? ", "What command should be run to run tests ?", "What does the user need to know about using the repo ? ", "Contributing to the repo ?"];
+const [username, emailId, title, description, license, dependencies, tests, usage, Contribution] = questions;
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+ }
 
 // TODO: Create a function to initialize app
 function init() {
-  
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'username',
+                message: username,
+            },
+            {
+                type: 'input',
+                name: 'emailId',
+                message: emailId,
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: title,
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: description,
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: license,
+
+                choices: [
+                    { name: 'MIT' },
+                    { name: 'APACHE 2.0' },
+                    { name: 'GPL 3.0' },
+                    { name: 'BSD 3' },
+                    { name: 'none' },
+                ]
+            },
+            {
+                type: 'input',
+                name: 'dependencies',
+                message: dependencies,
+            },
+            
+            {
+              type: 'input',
+              name: 'test',
+              message:tests,
+            },
+            
+            {
+              type: 'input',
+              name: 'usage',
+              message: usage,
+            },
+            {
+                type: 'input',
+                name: 'Contribution',
+                message: Contribution,
+              },
+        ])
+    // .then((data) => {
+    //   const readMePageContent = generateMarkdown(data);
+
+    //   fs.writeFile('README.md', readMePageContent, (err) =>
+    //     err ? console.log(err) : console.log('Successfully created README .!')
+    //   );
+    // })
 }
 
 // Function call to initialize app
