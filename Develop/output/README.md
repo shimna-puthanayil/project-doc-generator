5633d315598ae3bf08f60f10c57e475087584fd6
shimna <chimmu.p@gmail.com>
2023-07-20 17:12:18 +1000

    updated the code

---
+++ Develop/output/README.md
@@ -0,0 +1,49 @@
+# jbj
+  
+  
+
+  ## Description
+  
+  jbj
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#license)
+  * [Tests](#license)
+  * [Questions](#license)
+
+  ## Installation
+  
+   To install  necessary dependencies, run the following command :
+
+  ```
+  .
+  ```
+ 
+  ## Usage
+
+  bj
+
+  ## License
+
+  N/A 
+
+  ## Contributing
+
+  bn
+
+  ## Tests
+
+  To run tests, run the following command :
+
+  ```
+  kk
+  ```
+
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at hgh. You can find more of my work at [jhj](https://github.com/jhj).
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+//a function that returns a license badge based on which license is passed in
+
 function renderLicenseBadge(license) {
   let badge = ``;
   switch (license) {
@@ -22,9 +22,9 @@ function renderLicenseBadge(license) {
   return badge;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { 
+// a function that returns the license link
+
+function renderLicenseLink(license) {
   let licenseLink = ``;
   switch (license) {
     case "MIT":
@@ -46,35 +46,29 @@ function renderLicenseLink(license) {
   return licenseLink;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { 
-  let licenseText=`N/A`;
-  if(renderLicenseLink(license))
-  {
-    link=renderLicenseLink(license);
-    licenseText=`This project is licensed under the ${license} license\n ${link}`;
-}
+// a function that returns the license section of README
+
+function renderLicenseSection(license) {
+  let licenseText = `N/A`;
+  if (renderLicenseLink(license)) {
+    link = renderLicenseLink(license);
+    licenseText = `This project is licensed under the [${license}](${link}) license`;
+  }
   return licenseText;
 }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  if (renderLicenseBadge(data.license)) {
-    badge = renderLicenseBadge(data.license)
-    
-  }
-  else {
-    badge = ``;
-  }
- 
-  // link=renderLicenseLink(data.license);
-license=renderLicenseSection(data.license);
+// a function to generate markdown for README
 
+function generateMarkdown(data) {
+  let badgeLink = ``;
+  if (renderLicenseBadge(data.license)) {
+    badgeLink = `[![GitHub license](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})`;
+  }
+  license = renderLicenseSection(data.license);
   return `# ${data.title}
-  ![GitHub license](${badge})
   
- 
+  ${badgeLink}
+
   ## Description
   
   ${data.description}
@@ -102,11 +96,11 @@ license=renderLicenseSection(data.license);
 
   ## License
 
- ${license} 
+  ${license} 
 
   ## Contributing
 
-${data.Contribution}
+  ${data.Contribution}
 
   ## Tests
 
